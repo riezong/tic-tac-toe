dc77f17b36d8968b7355853139e5b89f1da8c03c
Andrew <71823699+riezong@users.noreply.github.com>
2025-02-17 12:53:19 +1100

    Add player factory function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function Gameboard() {
+    
+    // Create array to represent the board
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -6,17 +8,42 @@ function Gameboard() {
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
-        board[i].push(Cell());
+            board[i].push(0);
         }
         board.push[i];
     }
 
-    function Cell() {
+    function getBoard() {
+        return console.log(board);
+    }
+
+    function setCell(column, row, playerMark) {
+        // let targetCell = board[row][column];
+        // console.log(targetCell);
+        // if (targetCell === 0) {
+        //     console.log("empty");
+        //     board[row][column] = 1;
+        //     console.log(board);
+        //     endTurn();
+        // } else {
+        //     console.log("can't do that");
+        // }
         return 0;
     }
 
-    console.log(board);
-    return;
+    function resetBoard() {
+
+    }
+
+    getBoard();
 }
 
 Gameboard();
+
+function player(name) {
+    const mark = name.charAt(0);
+    return { name, mark };
+}
+
+console.log(player("Andrew"));
+console.log(player("Computer"));
